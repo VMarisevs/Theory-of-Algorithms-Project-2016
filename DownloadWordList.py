e1c29189057c045e9d86c6faea47c8058b5c4bfc
Vladislavs Marisevs <vmarisevs@gmail.com>
2016-02-19 23:13:15 +0000

    parsing allwords.txt file and creates words list file

--- DownloadWordList.py
+++ DownloadWordList.py
@@ -35,64 +35,28 @@ def downloadWordList(link):
 	output.close()
 	
 	
-	#print get_page(link)
-	#import urllib2
+def parseFile(filename):
 	#
-	#src = ""
+	# Reading allwords.txt file
+	# parsing and saving into words.txt file
 	#
-	#req = urllib2.Request(link)
-	#
-	#f = open("words.zip", 'wb')
-	#
-	#try:
-	#	response = urllib2.urlopen(req)
-	#	chunk = True
-	#	while chunk:
-	#		chunk = response.read(1024)
-	#		src += chunk
-	#		f.write(chunk)
-	#	response.close()
-	#except IOError:
-	#	print 'can\'t open', urllib2
-	#	return src
-	#
-	#f.close()
+	fileRead = open(filename, 'r')
 	
+	fileWrite = open('words.txt','wb')
 	
-	##http://stackoverflow.com/questions/22676/how-do-i-download-a-file-over-http-using-python
-	#import urllib2
-    #
-	#url = "http://www.bestwordlist.com/allwords.txt"
-    #
-	#file_name = url.split('/')[-1]
-	#u = urllib2.urlopen(url)
-	#f = open(file_name, 'wb')
-	#meta = u.info()
-	#file_size = int(meta.getheaders("Content-Length")[0])
-	#print "Downloading: %s Bytes: %s" % (file_name, file_size)
-    #
-	#file_size_dl = 0
-	#block_sz = 8192
-	#while True:
-	#	buffer = u.read(block_sz)
-	#	if not buffer:
-	#		break
-    #
-	#	file_size_dl += len(buffer)
-	#	f.write(buffer)
-	#	status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-	#	status = status + chr(8)*(len(status)+1)
-	#	print status,
-    #
-	#f.close()
-
-
+	counter = 0;
+	for line in fileRead:
+		if counter != 0:
+			fileWrite.write(line.replace(" ", "\n").lower())
+		else:
+			print line
+		counter+=1
 	
-def writeToFile(content):	
-	file = open('ListOfWords.txt', 'w')
-	file.write(content)
-	file.close()
+	fileWrite.close()
+	fileRead.close()
+	
 	
 if __name__ == "__main__":
-	downloadWordList("http://www.bestwordlist.com/allwords.txt")
+	#downloadWordList("http://www.bestwordlist.com/allwords.txt")
 	#downloadWordList("http://www-01.sil.org/linguistics/wordlists/english/")
+	parseFile("allwords.txt")
